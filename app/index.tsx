d88be942701c70721b2371942c2dd48fd51a1032
Nana Mohamadi <adilnana78@gmail.com>
2025-05-17 15:32:46 +0000

    Styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-export default function Index() {
+const HomeScreen = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>HomeScreen</Text>
     </View>
-  );
+  )
 }
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
