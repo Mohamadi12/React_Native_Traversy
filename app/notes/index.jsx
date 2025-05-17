885b2774dcd11936edab19b1adf25ed164390d88
Nana Mohamadi <adilnana78@gmail.com>
2025-05-17 16:27:18 +0000

    Display Notes In FlatList

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -1,20 +1,61 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
+import { FlatList } from "react-native-web";
 
 const NotesScreen = () => {
+  const [notes, setNotes] = useState([
+    { id: "1", text: "Note One" },
+    { id: "2", text: "Note Two" },
+    { id: "3", text: "Note Three" },
+  ]);
   return (
     <View style={styles.container}>
-      <Text>NotesScreen</Text>
-    </View>
-  )
-}
+      <FlatList
+        data={notes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.noteItem}>
+            <Text style={styles.noteText}>{item.text}</Text>
+          </View>
+        )}
+      />
 
-export default NotesScreen
+      <TouchableOpacity style={styles.addButton}>
+        <Text style={styles.addButtonText}>+ Add Note</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default NotesScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20,
-        backgroundColor: "#fff"
-    }
-})
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  noteItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#f5f5f5",
+    padding: 15,
+    borderRadius: 5,
+    marginVertical: 5,
+  },
+  noteText: {
+    fontSize: 18
+  },
+  addButton: {
+    backgroundColor: "#007bff",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  addButtonText: {
+   color: "#fff",
+   fontWeight: "bold",
+   fontSize: 18
+  }
+});
